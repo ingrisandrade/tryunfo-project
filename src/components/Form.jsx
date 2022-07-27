0deb43b024e278b93ef0ea13ada8ea0f2913af5a
Ingris Andrade <iac22@outlook.com>
2022-07-27 11:31:49 -0300

    add dois campos para texto com data-testid

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <p> </p>
+      <div>
+        <label htmlFor="name-input">
+          <input type="text" data-testid="name-input" name="name-input" />
+        </label>
+        <label htmlFor="description-input">
+          <textarea
+            name="description-input"
+            id=""
+            cols="30"
+            rows="10"
+            data-testid="description-input"
+          />
+        </label>
+      </div>
     );
   }
 }
