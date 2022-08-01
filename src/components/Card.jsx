486d143efefc107c499dec9a09e98c1fecf7c18d
Ingris Andrade <iac22@outlook.com>
2022-08-01 11:51:49 -0300

    refatora as props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends Component {
-  const = {
-    cardName,
-    cardDescription,
-    cardAttr1,
-    cardAttr2,
-    cardAttr3,
-    cardImage,
-    cardRare,
-    cardTrunfo,
-  } = this.props;
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
     return (
       <div>
-
+        
       </div>
     );
   }
