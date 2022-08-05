30b5b47fc9d3a337e51acf7d85a2ab2046aa5580
Ingris Andrade <iac22@outlook.com>
2022-08-05 01:44:31 -0300

    valida o botão salvar

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,9 @@ class App extends React.Component {
     cardTrunfo: '', /* boolean */
     // hasTrunfo, /* boolean */
     isSaveButtonDisabled: 'true', /* boolean */
-  }
+  };
 
-  onInputChange = ({ target }) => {
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [target.name]: value,
-    }, () => {
-      this.validateInputForm();
-    });
-  }
-
-  validateInputForm = () => {
+  handleButtonDisabled = () => {
     const {
       cardName,
       cardDescription,
@@ -33,8 +24,43 @@ class App extends React.Component {
       cardAttr2,
       cardAttr3,
       cardImage,
-      cardRare,
     } = this.state;
+
+    const numAttr1 = Number(cardAttr1);
+    const numAttr2 = Number(cardAttr2);
+    const numAttr3 = Number(cardAttr3);
+
+    const validateForm = cardName.length > 0
+      && cardDescription.length > 0
+      && cardAttr1.length > 0
+      && cardAttr2.length > 0
+      && cardAttr3.length > 0
+      && cardImage.length > 0;
+
+    const attrMaxValue = 90;
+    const attrMinValue = 0;
+
+    const attrs = cardAttr1 >= attrMinValue && cardAttr1 <= attrMaxValue
+      && cardAttr2 >= attrMinValue && cardAttr2 <= attrMaxValue
+      && cardAttr3 >= attrMinValue && cardAttr3 <= attrMaxValue;
+
+    const sumLimit = 210;
+    const attrsSumValid = numAttr1 + numAttr2 + numAttr3 <= sumLimit;
+
+    const all = validateForm && attrs && attrsSumValid;
+
+    this.setState({
+      isSaveButtonDisabled: !all,
+    });
+  }
+
+  onInputChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [target.name]: value,
+    }, () => {
+      this.handleButtonDisabled();
+    });
   }
 
   onSaveButtonClick = () => {
