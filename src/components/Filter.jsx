dff4317f18a1b366a53b09d29e85faef2b4a926c
Ingris Andrade <iac22@outlook.com>
2022-08-17 13:15:51 -0300

    Add campo do tipo text com data-testid

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class Filter extends Component {
   render() {
     return (
-      <div>Filter</div>
+      <div>
+        <label htmlFor="filter-name">
+          Lista de Cartas Filtradas
+          <input
+            type="text"
+            name="filter "
+            id="filter-name"
+            placeholder="Digite o nome da carta"
+            data-testid="name-filter"
+          />
+        </label>
+      </div>
     );
   }
 }
